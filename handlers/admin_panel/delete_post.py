73d8af0eef93bbb7b673c038a8bb6f656c102817
roadtodevops <roadtodevops@yandex.ru>
2022-11-17 17:59:14 +0500

    update main_menu.py, delete_post.py, create_post.py

--- handlers/admin_panel/delete_post.py
+++ handlers/admin_panel/delete_post.py
@@ -3,7 +3,7 @@ from config.bot_config import dp, bot
 from keyboards.admin_panel_keyboard_back_to_main_menu import admin_panel_keyboard_back_to_main_menu
 
 
-@dp.callback_query_handler(text='delete post')
+@dp.callback_query_handler(text='delete_post')
 async def admin_panel_delete_post_callback(callback_query: types.CallbackQuery):
     # Удаляем предыдущие сообщения
     await bot.delete_message(chat_id=callback_query.from_user.id,
--- handlers/admin_panel/main_menu.py
+++ handlers/admin_panel/main_menu.py
@@ -2,7 +2,7 @@ from aiogram import types
 from config.bot_config import dp, bot
 from keyboards.admin_panel_keyboard_main_menu import admin_panel_keyboard_main_menu
 
-@dp.callback_query_handler(text='main menu')
+@dp.callback_query_handler(text='main_menu')
 async def admin_panel_main_menu_callback(callback_query: types.CallbackQuery):
     # Удаляем предыдущие сообщения
     await bot.delete_message(chat_id=callback_query.from_user.id,
