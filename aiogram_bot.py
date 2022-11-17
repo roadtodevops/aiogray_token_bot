c21cd2d4fafbc0f27818f8cbadb8ad5a81706fc6
roadtodevops <roadtodevops@yandex.ru>
2022-11-17 16:28:42 +0500

    add bot codes

--- aiogram_bot.py
+++ aiogram_bot.py
@@ -1,16 +1,24 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+from dotenv import dotenv_values
 
-logging.basicConfig(level=logging.INFO)
+#Получение токена
+config = dotenv_values(".env")
+API_TOKEN = config['API_TOKEN']
 
-API_TOKEN = '5630975781:AAEORceYCE8oCzISglCYQdSDjaYv_AMQURU'
+#Настройка логов
+logging.basicConfig(level=logging.DEBUG)
+
+#Запуск бота и диспатчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+#Запуск приветствия
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("""Привет!\nЯ бот команды инженеров Sitronics\nДля просмотра моего функционала, введи свою доменную почту и подтверди её :)""")
 
+#
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
     await message.reply("""пока пусто""")
